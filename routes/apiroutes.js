5e14b9e581a568b6c22b3e7ad37868d251534de4
Chris Mims <chritopherm970@aol.com>
2020-07-24 13:04:23 -0500

    Added app delete and post func but not working at the moment.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -10,9 +10,39 @@ module.exports = function (app){
                 return console.log(err);
             }
             var parsed = JSON.parse(data)
-            console.log(parsed)
+            
 
             res.json(parsed)
         })
     })
+
+    app.post("/api/notes", function(req, res){
+        var newNote = req.body
+        fs.readFile(dbPath, "utf8", function(err, data){
+            if(err){
+                return console.log(err);
+            }
+            var parsed =JSON.parse(data)
+            var lastIndex = parsed.length - 1
+            var lastId = parsed[lastIndex].id
+            newNote.id = lastId + 1
+            parsed.push(newNote)
+            //might need to push new note to parsed then write parsed to file
+            fs.writeFile(dbPath, "utf8", JSON.stringify(newNote), function(req, res){
+                res.json(newNote)
+            })
+        })
+    }
+    )
+
+    app.delete("/api/notes/:id"), function(req, res){
+        var id = req.params.id;
+        dbPath.splice(id - 1, 1);
+        dbPath.forEach(function(obj, i){
+            obj.id = i + 1;
+        })
+        fs.writeFile(dbPath, JSON.stringify(newNote), function(){
+            res.json(newNote)
+        })
+    }
 }

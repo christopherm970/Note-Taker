bb4ed8962495f66aff07645e28d938a6b9b49c95
Chris Mims <chritopherm970@aol.com>
2020-07-24 21:02:26 -0500

    Fixed issue with app.post

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -9,32 +9,47 @@ module.exports = function (app){
             if(err){
                 return console.log(err);
             }
-            var parsed = JSON.parse(data)
-            
+            var parsed = JSON.parse(data);
+            console.log(parsed)
 
             res.json(parsed)
         })
     })
-
     app.post("/api/notes", function(req, res){
-        var newNote = req.body
+    
+        var newNote = req.body;
+
         fs.readFile(dbPath, "utf8", function(err, data){
-            if(err){
-                return console.log(err);
-            }
-            var parsed =JSON.parse(data)
-            var lastIndex = parsed.length - 1
-            var lastId = parsed[lastIndex].id
-            newNote.id = lastId + 1
-            parsed.push(newNote)
-            //might need to push new note to parsed then write parsed to file
-            fs.writeFile(dbPath, "utf8", JSON.stringify(newNote), function(req, res){
-                res.json(newNote)
+            var noteList = JSON.parse(data);
+            var lastIndex = noteList.length - 1;
+            var lastId = parseInt(noteList[lastIndex].id);
+            newNote.id = lastId + 1;
+            noteList.push(newNote);
+
+            fs.writeFile(dbPath, JSON.stringify(noteList), function(req, res){
+                console.log("success!");
             })
         })
-    }
-    )
 
+        
+
+
+        //fs.readFile(dbPath, "utf8", function(err, data){
+        //    if(err){
+        //        return console.log(err);
+        //    }
+            //console.log(data);
+            //var parsed =JSON.parse(data);
+            //var lastIndex = parsed.length - 1;
+            //var lastId = parsed[lastIndex].id
+            //newNote.id = lastId + 1
+            //parsed.push(newNote);
+            //might need to push new note to parsed then write parsed to file
+            //fs.writeFile(dbPath, "utf8", JSON.stringify(newNote), function(req, res){
+            //    res.json(newNote);
+            //})
+        //})
+    })
     app.delete("/api/notes/:id"), function(req, res){
         var id = req.params.id;
         dbPath.splice(id - 1, 1);
@@ -45,4 +60,4 @@ module.exports = function (app){
             res.json(newNote)
         })
     }
-}
+} 

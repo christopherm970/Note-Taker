26ff0b3b4fc05dbf24eb42aa46ce8738886d6187
Chris Mims <chritopherm970@aol.com>
2020-07-25 12:58:41 -0500

    Updated read me and added example gif.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Note-Taker
+
+## Description
+Application that allows a user to input a note and either save or delete dynamically. Application is deployed and ran through heroku.
+
+## Installation
+To run locally on user's machine git clone and npm install. Application is also deployed on heroku [Click here](https://glacial-meadow-27138.herokuapp.com/) 
+
+## Example
+![Demo](noteExamp.gif)
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -38,21 +38,6 @@ module.exports = function (app){
             })
             res.json(noteList);
         })
-        //fs.readFile(dbPath, "utf8", function(err, data){
-        //    if(err){
-        //        return console.log(err);
-        //    }
-            //console.log(data);
-            //var parsed =JSON.parse(data);
-            //var lastIndex = parsed.length - 1;
-            //var lastId = parsed[lastIndex].id
-            //newNote.id = lastId + 1
-            //parsed.push(newNote);
-            //might need to push new note to parsed then write parsed to file
-            //fs.writeFile(dbPath, "utf8", JSON.stringify(newNote), function(req, res){
-            //    res.json(newNote);
-            //})
-        //})
     })
     app.delete("/api/notes/:id", function(req, res){
         
